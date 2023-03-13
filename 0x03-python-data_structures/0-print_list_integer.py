0ad73d6b92d0c5ff680f976495e25d9675b64e59
oremu <oremuokozi@gmail.com>
2023-03-13 16:18:14 +0530

    corrections

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for list in range(len(my_list)):
-        print("{:d}".fprmat(my_list[list]))
+    for i in my_list:
+        print("{:d}".format(i))
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -18,7 +20,7 @@ listint_t;
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-size_t listint_len(const listint_t **head);
+int is_palindrome(listint_t **head);
+int checkPalindrome(listint_t **headptr, listint_t *tptr);
 
 #endif
