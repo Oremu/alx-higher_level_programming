ad1d6a2fda7765c3da981d889fb98e417a8ebdf9
oremu <oremuokozi@gmail.com>
2023-03-28 06:58:27 -0700

    main tasks

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Declares a square class"""
+
+    def __init_(self, size=0) -> None:
+        """
+        Initializes class attributes
+
+        Args:
+            size: size of square
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("sie must be >= 0")
+        else:
+            self.__size = size
