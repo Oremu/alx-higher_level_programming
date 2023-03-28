e81e05c1291b34f2b6d6fb5a42ef7f3a335b47e0
oremu <oremuokozi@gmail.com>
2023-03-28 07:09:28 -0700

    main tasks

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
             size: size of square
         """
 
-        if type(size) is not int:
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
